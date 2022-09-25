4236f7f8fc6cdb6566b88277e1c67f6d50bec900
Pedro Itaboray <pedroh_itaboray@hotmail.com>
2022-09-25 10:32:15 -0300

    Integração com api de criar usuarios

--- CW2_WebClient/Pages/Usuarios.cshtml.cs
+++ CW2_WebClient/Pages/Usuarios.cshtml.cs
@@ -1,6 +1,8 @@
 using CW2_WebClient.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace CW2_WebClient.Pages
 {
@@ -8,8 +10,56 @@ namespace CW2_WebClient.Pages
     {
         [BindProperty]
         public Usuario? usuario { get; set; }
-        public void OnGet()
+
+        [TempData]
+        public bool sucessoCadastro { get; set; } = false;
+
+        public string Username { get; set; }
+        public IActionResult OnGet()
         {
+            Username = HttpContext.Session.GetString("usuario");
+
+            if (string.IsNullOrWhiteSpace(Username))
+            {
+                return Redirect("/login");
+            }
+
+            sucessoCadastro = false;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync([FromServices] IConfiguration config)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                string baseURL =
+                    config.GetSection("API_Usuario:BaseURL").Value;
+
+                var myContent = JsonConvert.SerializeObject(usuario);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+                var byteContent = new ByteArrayContent(buffer);
+
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                var response = await client.PostAsync(baseURL, byteContent);
+
+                response.EnsureSuccessStatusCode();
+                sucessoCadastro = true;
+
+                string conteudo =
+                    response.Content.ReadAsStringAsync().Result;
+            }
+
+            return Page();
         }
     }
 }
